800793465bba1a5a757aa6d0c9a4522af0fa5ec6
Cheng-Yang Fu <cyfu@cs.unc.edu>
2018-01-05 09:24:23 -0500

    Update main.py
    fix the error in the comment.

--- main.py
+++ main.py
@@ -268,7 +268,7 @@ class AverageMeter(object):
 
 
 def adjust_learning_rate(optimizer, epoch):
-    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    """Sets the learning rate to the initial LR decayed by 2 every 30 epochs"""
     lr = args.lr * (0.5 ** (epoch // 30))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
